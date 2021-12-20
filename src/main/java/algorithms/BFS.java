c944b790e3c92133ec504b8bb4250100fafc64c4
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2021-12-20 18:09:01 +0100

    BFS. Adding shortest path solution using BFS algorithm. Adding 3 different graphs to unit test.

--- src/main/java/algorithms/BFS.java
+++ src/main/java/algorithms/BFS.java
@@ -1,5 +1,9 @@
 package algorithms;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -41,5 +45,38 @@ public class BFS {
             }
         }
         return result;
+    }
+
+    public static int shortestPathGraph(Map<Character, Character[]> graph, char source, char destination) {
+        var queue = new ConcurrentLinkedQueue<NodeDistancePair>();
+        var visited = new HashSet<Character>();
+        var initialPosition = 0;
+        var result = -1;
+
+        queue.add(new NodeDistancePair(source, initialPosition));
+
+        while(!queue.isEmpty()) {
+            var currentPair = queue.poll();
+            if(currentPair.getNode() == destination) {
+                result = currentPair.getDistance();
+                break;
+            } else {
+                if(!visited.contains(currentPair.getNode())) {
+                    visited.add(currentPair.getNode());
+                    var newDistance = currentPair.getDistance() + 1;
+                    for(char c : graph.get(currentPair.getNode())) {
+                        if(!visited.contains(c)) queue.add(new NodeDistancePair(c, newDistance));
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    private static class NodeDistancePair {
+        private char node;
+        private int distance;
     }
 }
--- src/test/java/algorithms/BFSTest.java
+++ src/test/java/algorithms/BFSTest.java
@@ -1,13 +1,16 @@
 package algorithms;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
 
-import static algorithms.BFS.charGraph;
-import static algorithms.BFS.hasPathGraph;
+import static algorithms.BFS.*;
+import static graphs.EdgeFactory.createUndirectedEdgePath;
+import static graphs.EdgeFactory.createdShortestPathEdgePath;
 import static graphs.GraphFactory.*;
+import static mapper.GraphMapper.mapEdgePathToGraph;
+import static mapper.GraphMapper.mapGraphWithListToGraphWithArray;
+import static org.junit.Assert.*;
 
 public class BFSTest {
 
@@ -24,10 +27,39 @@ public class BFSTest {
         var graph = createHasPathGraph();
         var source = 'f';
         var destination = 'k';
-        Assert.assertTrue(hasPathGraph(graph, source, destination));
+        assertTrue(hasPathGraph(graph, source, destination));
 
         source = 'j';
         destination = 'f';
-        Assert.assertFalse(hasPathGraph(graph, source, destination));
+        assertFalse(hasPathGraph(graph, source, destination));
+    }
+
+    @Test
+    public void bfsShortestPathGraphTest() {
+        var edge = createdShortestPathEdgePath();
+        var graphWithList = mapEdgePathToGraph(edge);
+        var graph = mapGraphWithListToGraphWithArray(graphWithList);
+        var source = 'w';
+        var destination = 'z';
+
+        assertEquals(2, shortestPathGraph(graph, source, destination));
+
+        graph = createHasPathGraph();
+        source = 'f';
+        destination = 'h';
+        assertEquals(2, shortestPathGraph(graph, source, destination));
+        destination = 'j';
+        assertEquals(-1, shortestPathGraph(graph, source, destination));
+
+        edge = createUndirectedEdgePath();
+        graphWithList = mapEdgePathToGraph(edge);
+        graph = mapGraphWithListToGraphWithArray(graphWithList);
+        source = 'i';
+        destination = 'm';
+        assertEquals(2, shortestPathGraph(graph, source, destination));
+        destination = 'j';
+        assertEquals(1, shortestPathGraph(graph, source, destination));
+        destination = 'a';
+        assertEquals(-1, shortestPathGraph(graph, source, destination));
     }
 }
