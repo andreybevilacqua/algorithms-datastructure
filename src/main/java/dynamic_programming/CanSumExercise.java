125c71b75fa840d7ec75d7ba2e4c7de2e3545228
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2022-06-02 17:58:44 +0100

    DP. Refactoring CanSum to use both with and without memo.

--- src/main/java/dynamic_programming/CanSumExercise.java
+++ src/main/java/dynamic_programming/CanSumExercise.java
@@ -4,14 +4,27 @@ import java.util.HashMap;
 
 public class CanSumExercise {
 
-    public static boolean canSum(int target, int[] ints, HashMap<Integer, Boolean> memo) {
+    public static boolean canSumWithoutMemo(int target, int[] ints) {
+        if(target == 0) return true;
+        var result = false;
+        for(int i : ints) {
+            var diff = target - i;
+            if(diff >= 0) {
+                result = canSumWithoutMemo(diff, ints);
+                if(result) break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean canSumWithMemo(int target, int[] ints, HashMap<Integer, Boolean> memo) {
         if(target == 0) return true;
         if(memo.containsKey(target)) return memo.get(target);
         var result = false;
         for(int i : ints) {
             var diff = target - i;
             if(diff >= 0) {
-                result = canSum(diff, ints, memo);
+                result = canSumWithMemo(diff, ints, memo);
                 memo.put(diff, result);
                 if(result) break;
             }
--- src/test/java/dynamic_programming/CanSumExerciseTest.java
+++ src/test/java/dynamic_programming/CanSumExerciseTest.java
@@ -4,19 +4,36 @@ import org.junit.Test;
 
 import java.util.HashMap;
 
-import static dynamic_programming.CanSumExercise.canSum;
+import static dynamic_programming.CanSumExercise.canSumWithMemo;
+import static dynamic_programming.CanSumExercise.canSumWithoutMemo;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class CanSumExerciseTest {
 
     @Test
-    public void test1() {
-        assertTrue(canSum(3, new int[]{1,2}, new HashMap<>()));
-        assertTrue(canSum(7, new int[]{5,3,4,7}, new HashMap<>()));
-        assertTrue(canSum(8, new int[]{2,3,5}, new HashMap<>()));
-        assertFalse(canSum(3, new int[]{2,2}, new HashMap<>()));
-        assertFalse(canSum(7, new int[]{8,9}, new HashMap<>()));
-        assertFalse(canSum(300, new int[]{7,14}, new HashMap<>()));
+    public void testWithoutMemo() {
+        var start = System.currentTimeMillis();
+        assertTrue(canSumWithoutMemo(3, new int[]{1,2}));
+        assertTrue(canSumWithoutMemo(7, new int[]{5,3,4,7}));
+        assertTrue(canSumWithoutMemo(8, new int[]{2,3,5}));
+        assertFalse(canSumWithoutMemo(3, new int[]{2,2}));
+        assertFalse(canSumWithoutMemo(7, new int[]{8,9}));
+        assertFalse(canSumWithoutMemo(300, new int[]{7,14}));
+        var end = System.currentTimeMillis();
+        System.out.println("Total time: " + (end - start));
+    }
+
+    @Test
+    public void testWithMemo() {
+        var start = System.currentTimeMillis();
+        assertTrue(canSumWithMemo(3, new int[]{1,2}, new HashMap<>()));
+        assertTrue(canSumWithMemo(7, new int[]{5,3,4,7}, new HashMap<>()));
+        assertTrue(canSumWithMemo(8, new int[]{2,3,5}, new HashMap<>()));
+        assertFalse(canSumWithMemo(3, new int[]{2,2}, new HashMap<>()));
+        assertFalse(canSumWithMemo(7, new int[]{8,9}, new HashMap<>()));
+        assertFalse(canSumWithMemo(300, new int[]{7,14}, new HashMap<>()));
+        var end = System.currentTimeMillis();
+        System.out.println("Total time: " + (end - start));
     }
 }
