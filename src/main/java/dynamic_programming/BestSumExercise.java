2ba042aaed9dd64b8805ef00f0dca4363db68e78
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2022-06-03 18:52:34 +0100

    DP. Adding complexity cost.

--- src/main/java/dynamic_programming/BestSumExercise.java
+++ src/main/java/dynamic_programming/BestSumExercise.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 public class BestSumExercise {
 
+    // Brute force solution without memoization
+    // O(n^m) time
+    // O(m^2) space
     public static List<Integer> bestSumWithoutMemo(int target, int[] ints) {
         if(target == 0) return new ArrayList<>();
         List<Integer> shortestCombination = null;
@@ -25,6 +28,9 @@ public class BestSumExercise {
         return shortestCombination;
     }
 
+    // Memoization implemented
+    // O(n * m^2) time, because of 'var combination = new ArrayList<>(remainderCombination);'
+    // O(m^2) space
     public static ArrayList<Integer> bestSumWithMemo(int target, int[] ints, HashMap<Integer, ArrayList<Integer>> memo) {
         if(target == 0) return new ArrayList<>();
         if(memo.containsKey(target)) return memo.get(target);
