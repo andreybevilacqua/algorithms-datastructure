682bb5fd6de2f15c0fce8f91be49fd8e28949871
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2022-06-16 13:18:23 +0100

    DP.
    Adding CanConstruct exercise recursive solution with memorization technique.

--- src/main/java/dynamic_programming/string_exercises/CanConstructExercise.java
+++ src/main/java/dynamic_programming/string_exercises/CanConstructExercise.java
@@ -1,4 +1,22 @@
 package dynamic_programming.string_exercises;
 
+import java.util.HashMap;
+
 public class CanConstructExercise {
+
+    public static boolean canConstruct(String target, String[] wordBank, HashMap<String, Boolean> memo) {
+        if(target.isEmpty()) return true;
+        if(memo.containsKey(target)) return memo.get(target);
+        for(String word : wordBank) {
+            if(target.indexOf(word) == 0) {
+                var suffix = target.substring(word.length());
+                if(canConstruct(suffix, wordBank, memo)) {
+                    memo.put(target, true);
+                    return true;
+                }
+            }
+        }
+        memo.put(target, false);
+        return false;
+    }
 }
--- src/test/java/dynamic_programming/string_exercises/CanConstructExerciseTest.java
+++ src/test/java/dynamic_programming/string_exercises/CanConstructExerciseTest.java
@@ -1,4 +1,20 @@
 package dynamic_programming.string_exercises;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static dynamic_programming.string_exercises.CanConstructExercise.canConstruct;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class CanConstructExerciseTest {
+
+    @Test
+    public void test1() {
+        assertTrue(canConstruct("abcdef", new String[]{"ab","abc","cd","def","abcd"}, new HashMap<>()));
+        assertFalse(canConstruct("skateboard", new String[]{"bo","rd","ate","t","ska", "sk", "boar"}, new HashMap<>()));
+        assertTrue(canConstruct("enterapotentpot", new String[]{"a","p","ent","enter","ot", "o", "t"}, new HashMap<>()));
+        assertFalse(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", new String[]{"e","ee","eee","eeee","eeeee", "eeeeee"}, new HashMap<>()));
+    }
 }
